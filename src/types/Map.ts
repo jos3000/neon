2bc7c6e7e470cee2b24f87da377bb04055ebc082
Jos <jos@toughside.co.uk>
2026-08-03 20:39:46 +0100

    Example Types

---
+++ src/types/Map.ts
@@ -0,0 +1,13 @@
+export type Wall = { x: number; y: number; w: number; h: number };
+
+export interface Map {
+  name: string;
+  width: number;
+  height: number;
+  walls: Wall[];
+  enemies: {
+    id: string;
+    x: number;
+    y: number;
+  }[];
+}
